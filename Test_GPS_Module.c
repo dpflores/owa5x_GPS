120bdc19dec535e1d4065783d5b3473f7f5ee52f
Del Piero Flores <delpier22.flores@gmail.com>
2023-07-25 12:45:10 -0500

    u

--- Test_GPS_Module.c
+++ Test_GPS_Module.c
@@ -30,6 +30,7 @@
 //System Includes
 //-----------------------------------------------------------------//
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <errno.h>
 #include <ctype.h>
